2988aa745b662e86bd477689dc9565520da747e4
Bastien Andrieu <bastien.andrieu@yahoo.fr>
2019-02-04 17:51:15 +0100

    modif figures validation vortex

--- memoire/figures/code/convergence_continuity.py
+++ memoire/figures/code/convergence_continuity.py
@@ -28,13 +28,20 @@ def fun(x, j):
     elif j == 2:
         f = np.zeros(len(x))
         l = np.where(x > 0)
-        f[l] = np.power(x[l],2)
+        f[l] = np.power(x[l],2)#3)
     elif j == 3:
         f = np.zeros(len(x))
         l = np.where(x > 0)
         f[l] = np.exp(1.-1./x[l])
+        """
+        a = 1.212
+        f = np.sqrt(1 - np.power(x/a, 2))
+        """
+        #f = x / np.sqrt(1.0 + np.power(x, 2))
     elif j == 4:
-        f = np.exp(np.sin(3.*np.power(x,3)) - 1.)
+        #f = np.exp(np.sin(3.*np.power(x,3)) - 1.)
+        #f = np.sqrt(1 - np.power(x/1.212, 2))
+        f = 1.0 / np.sqrt(1.0 + np.power(x, 2))
         #eps = 1e-16
         #a = -np.log(eps)
         #f = np.exp(-a*np.power(x,2))
@@ -48,17 +55,19 @@ def spectral_filter(c, p):
     f = np.exp(a*np.power(w,2*p))
     return c*f
 ##########################################
+eps = 2.2e-15
+
 x = np.linspace(-1.0,1.0,10000)
 y = np.zeros((5,len(x)))
 
-#fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 for j in range(5):
     y[j] = fun(x, j)
-    #ax.plot(x, y[j], label='j='+str(j+1))
-#ax.set_xlim([-1,1])
-#plt.grid()
-#plt.legend()
-#plt.show()
+    ax.plot(x, y[j], label='j='+str(j+1))
+ax.set_xlim([-1,1])
+plt.grid()
+plt.legend()
+plt.show()
 
 
 
@@ -69,16 +78,23 @@ g = 3
 #Nvec = np.rint(np.power(2, np.arange(1,10,0.5))).astype(int)
 Nvec = np.hstack([np.arange(10,200,10), np.power(2,np.arange(8,11))])
 err = np.zeros((5,len(Nvec)))
+est = np.zeros((5,len(Nvec)))
 coef = np.zeros((5,np.amax(Nvec)+1))
 for i, N in enumerate(Nvec):
     xN = cgl_nodes(N)
     for j in range(5):
         fN = fun(xN, j)
         c = fcht(fN)
-        if j < 2:
-            c = spectral_filter(c, 1)
+        #if j < 2:
+        #    c = spectral_filter(c, 1)
         z = chebval(x, c)
         err[j,i] = norminf(y[j] - z)
+        for k in range(N,-1,-1):
+            if np.absolute(c[k]) > eps:
+                est[j,i] = np.absolute(c[k])
+                if j < 3:
+                    est[j,i] *= k
+                break
         if i == len(Nvec)-1:
             coef[j] = np.absolute(c)
 
@@ -90,19 +106,25 @@ p = np.polyfit(X, Y, 1)
 q = p[0]
 print "q=",q
 
-X = Nvec[3:6]
-Y = np.log(err[4,3:6])
+#X = Nvec[3:6]
+#Y = np.log(err[4,3:6])
+X = Nvec[:3]
+Y = np.log(err[4,:3])
 p = np.polyfit(X, Y, 1)
 r = np.exp(-p[0])
 print 'r=',r
 
+cl = ['b','g','r','c','m']
+lwd = 1.5
+
 fig, ax = plt.subplots(nrows=1, ncols=2)
 for j in range(5):
-    ax[0].loglog(Nvec, err[j], '.-', label=lbl[j])
+    ax[0].loglog(Nvec, err[j], '.-', label=lbl[j], color=cl[j])
+    ax[0].loglog(Nvec, est[j], '+--', color=cl[j])
 
-ax[0].loglog(Nvec,np.power(Nvec,-1.),'k:', label='N^-1')
-ax[0].loglog(Nvec,np.power(Nvec,-2.),'k--', label='N^-2')
-ax[0].loglog(Nvec,np.exp(q*np.power(Nvec,b)), 'k-.', label='exp(qN^b)')
+ax[0].loglog(Nvec,np.power(Nvec,-1.),'k:', label='N^-1', lw=lwd)
+ax[0].loglog(Nvec,np.power(Nvec,-2.),'k--', label='N^-2', lw=lwd)
+ax[0].loglog(Nvec,np.exp(q*np.power(Nvec,b)), 'k-.', label='exp(qN^b)', lw=lwd)
 
 ax[0].set_xlabel('N')
 ax[0].set_ylim([1e-16,1e1])
@@ -110,10 +132,12 @@ ax[0].legend(loc='best')
 ax[0].grid()
 
 for j in range(5):
-    ax[1].semilogy(Nvec, err[j], '.-', label=lbl[j])
+    ax[1].semilogy(Nvec, err[j], '.-', label=lbl[j], color=cl[j])
+    ax[1].semilogy(Nvec, est[j], '+--', color=cl[j])
 
-#ax[1].semilogy(Nvec,np.exp(q*np.power(Nvec,b)), 'k:', label='exp(qN^b)')
-ax[1].semilogy(Nvec,np.power(r,-Nvec), 'k--', label='r^-N')
+ax[1].semilogy(Nvec,np.exp(q*np.power(Nvec,b)), 'k:', label='exp(qN^b)', lw=lwd)
+ax[1].semilogy(Nvec,np.power(r,-Nvec), 'k--', label='r^-N', lw=lwd)
+
 
 ax[1].set_xlabel('N')
 ax[1].set_xlim([0,100])
@@ -124,10 +148,14 @@ ax[1].grid()
 plt.show()
 
 
-"""
+
 fig, ax = plt.subplots(nrows=1, ncols=2)
 for j in range(5):
     ax[0].loglog(coef[j], '.', label=lbl[j])
+
+ax[0].loglog(Nvec,np.power(Nvec,-1.),'k:', label='N^-1', lw=lwd)
+ax[0].loglog(Nvec,np.power(Nvec,-2.),'k--', label='N^-2', lw=lwd)
+ax[0].loglog(Nvec,np.power(Nvec,-3.), 'k-.', label='N^-3', lw=lwd)
 
 ax[0].set_xlabel('n')
 ax[0].set_ylim([1e-16,1e1])
@@ -136,7 +164,9 @@ ax[0].grid()
 
 for j in range(5):
     ax[1].semilogy(coef[j], '.', label=lbl[j])
- 
+
+ax[1].semilogy(Nvec,np.power(r,-Nvec), 'k--', label='r^-N', lw=lwd)
+
 ax[1].set_xlabel('n')
 ax[1].set_xlim([0,100])
 ax[1].set_ylim([1e-16,1e1])
@@ -144,7 +174,7 @@ ax[1].legend(loc='best')
 ax[1].grid()
 
 plt.show()
-"""
+
 """
 for g in range(2,5):
     fig, ax = plt.subplots()
