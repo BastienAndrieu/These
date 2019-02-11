9a8c633b539165c00fcc3aa49f58a1fc71c9237d
Bastien Andrieu <bastien.andrieu@yahoo.fr>
2019-02-11 18:02:31 +0100

    fini figure canal surface (blender)

--- memoire/figures/code/fig_canal_surface2.py
+++ memoire/figures/code/fig_canal_surface2.py
@@ -10,7 +10,7 @@ import my_lib as myl
 
 
 def fr(x):
-    return 0.2*(1.0 + 1.5*x[0] + 0.2*x[1] - 0.6*x[2])
+    return 0.2*(1.0 + 1.2*x[0] + 0.2*x[1] - 0.6*x[2])
     
 
 b = np.array([
@@ -32,8 +32,11 @@ d2pg = cheblib.diff(dpg)
 
 m = 100
 n = 100
-#u = np.linspace(-1,1,m)
-u = np.linspace(-0.8,0.2,m)
+m1 = 10
+m2 = 60
+dm = 6
+u = np.linspace(-1,1,m)
+#u = np.linspace(-0.8,0.2,m)
 v = np.pi*np.linspace(-1,1,n+1)
 
 u0 = -0.5
@@ -50,9 +53,9 @@ r = chebval(u, pr)
 dr = chebval(u, dpr)
 
 # envelope
-e = np.zeros((m,n,3))
+e = np.zeros((m2-m1+1,n,3)) #np.zeros((m,n,3))
 bu0 = np.array([-1.,0.,0.15])
-for i in range(m):
+for i in range(m1,m2+1):
     normdg = np.sqrt(np.sum(np.power(dg[:,i],2)))
     sina = dr[i]/normdg
     occ = g[:,i] - r[i]*sina*dg[:,i]/normdg
@@ -63,24 +66,24 @@ for i in range(m):
     bu = bu / np.sqrt(np.sum(np.power(bu,2)))
     bv = np.cross(bw,bu)
     for j in range(n):
-        e[i,j] = occ + rcc*(np.cos(v[j])*bu + np.sin(v[j])*bv)
+        e[i-m1,j] = occ + rcc*(np.cos(v[j])*bu + np.sin(v[j])*bv)
 
 ##############################
 # clear blender scene
 myl.blankScene()
 
 # add spine curve
-myl.addPolyline(g.T, [0,0,0], 1.5e-3, 0)
+myl.addPolyline(g[:,m1-dm:m2+dm+1].T, [0,0,0], 1.5e-3, 0)
 bpy.data.materials["Mat_line"].use_transparency = True
 bpy.data.materials["Mat_line"].alpha = 0
 
 # add envelope ############
 verts = []
 faces = []
-for i in range(m):
+for i in range(e.shape[0]):
     for j in range(n):
         verts.append([e[i,j,0], e[i,j,1], e[i,j,2]])
-        if i < m-1:
+        if i < e.shape[0]-1:
             faces.append([i*n + j, i*n + (j+1)%n, (i+1)*n + (j+1)%n, (i+1)*n + j])
 #create mesh and object
 mesh = bpy.data.meshes.new("envelope")
@@ -240,20 +243,22 @@ az = (20.0 - 90.0)*np.pi/180.0
 el = 0.0#-5.0*np.pi/180.0
 xcam = np.array([1.0000, 0.1067, -0.3586])#g0
 rcam = 18.959#34.6*r0
-scene.camera.location = xcam + rcam*np.array([np.cos(az)*np.cos(el), np.sin(az)*np.cos(el), np.sin(el)])
-scene.camera.rotation_euler = np.array([89.98, 0.62, 379.9])*np.pi/180.0
+#scene.camera.location = xcam + rcam*np.array([np.cos(az)*np.cos(el), np.sin(az)*np.cos(el), np.sin(el)])
+scene.camera.location = [14.6387, -19.52351, 0.52857]
+#scene.camera.rotation_euler = np.array([89.98, 0.62, 379.9])*np.pi/180.0
+scene.camera.rotation_euler = np.array([87.975, 0.62, 393.916])*np.pi/180.0
 bpy.data.cameras["Camera"].lens_unit = "FOV"
 bpy.data.cameras["Camera"].angle = 6.609*np.pi/180.0
 
 
-scene.render.resolution_x = 800
-scene.render.resolution_y = 600
+scene.render.resolution_x = 1000
+scene.render.resolution_y = 0.75*scene.render.resolution_x
 scene.render.resolution_percentage = 100
 
 bpy.data.worlds["World"].horizon_color = [1,1,1]
 scene.render.use_border = False
 scene.render.use_freestyle = True
-#scene.render.alpha_mode = 'TRANSPARENT'
+scene.render.alpha_mode = 'TRANSPARENT'
 
 
 ############################################################
@@ -292,6 +297,10 @@ bpy.ops.scene.freestyle_lineset_add() #5
 bpy.ops.scene.freestyle_lineset_add() #6
 bpy.ops.scene.freestyle_lineset_add() #7
 bpy.ops.scene.freestyle_lineset_add() #8
+bpy.ops.scene.freestyle_lineset_add() #9
+bpy.ops.scene.freestyle_lineset_add() #10
+bpy.ops.scene.freestyle_lineset_add() #11
+bpy.ops.scene.freestyle_lineset_add() #12
 
 freestyle = scene.render.layers.active.freestyle_settings
 freestyle.use_smoothness = True
@@ -303,7 +312,9 @@ lineset.select_silhouette = False
 lineset.select_border = False
 lineset.select_crease = False
 lineset.select_by_group = True
-lineset.visibility = 'VISIBLE'
+lineset.visibility = 'RANGE'
+lineset.qi_start = 0
+lineset.qi_end = 1
 linestyle = bpy.data.linestyles["LineStyle"]
 linestyle.caps = "ROUND"
 linestyle.use_chaining = False
@@ -317,7 +328,9 @@ lineset.select_silhouette = False
 lineset.select_border = False
 lineset.select_crease = False
 lineset.select_by_group = True
-lineset.visibility = 'HIDDEN'
+lineset.visibility = 'RANGE'
+lineset.qi_start = 2
+lineset.qi_end = 100
 linestyle = bpy.data.linestyles["LineStyle.001"]
 linestyle.caps = "ROUND"
 linestyle.use_chaining = True
@@ -327,6 +340,7 @@ linestyle.dash1 = 5
 linestyle.gap1 = linestyle.dash1
 lineset.group = bpy.data.groups["ccircle_group"]
 
+pl_lw = 2.0
 # line set 3 (plane, border, visible)
 lineset = freestyle.linesets["LineSet 3"]
 lineset.select_silhouette = False
@@ -339,6 +353,7 @@ linestyle.caps = "ROUND"
 linestyle.use_chaining = False
 linestyle.color = cc_cl
 lineset.group = bpy.data.groups["plane_group"]
+linestyle.thickness = pl_lw
 
 # line set 4 (plane, border, visible)
 lineset = freestyle.linesets["LineSet 4"]
@@ -355,6 +370,7 @@ linestyle.use_dashed_line = True
 linestyle.dash1 = 5
 linestyle.gap1 = linestyle.dash1
 lineset.group = bpy.data.groups["plane_group"]
+linestyle.thickness = pl_lw
 
 en_cl = [0.252,0.776,0.088]
 # line set 5 (envelope, silhouette, visible)
@@ -379,6 +395,9 @@ lineset.visibility = 'HIDDEN'
 linestyle = bpy.data.linestyles["LineStyle.005"]
 linestyle.caps = "ROUND"
 linestyle.color = en_cl
+linestyle.use_dashed_line = True
+linestyle.dash1 = 5
+linestyle.gap1 = linestyle.dash1
 lineset.group = bpy.data.groups["envelope_group"]
 
 sp_cl = [0.799,0.396,0.159]
@@ -388,22 +407,89 @@ lineset.select_silhouette = True
 lineset.select_border = False
 lineset.select_crease = False
 lineset.select_by_group = True
-lineset.visibility = 'HIDDEN'
+lineset.visibility = 'RANGE'
+lineset.qi_start = 0
+lineset.qi_end = 1
 linestyle = bpy.data.linestyles["LineStyle.006"]
 linestyle.caps = "ROUND"
 linestyle.color = sp_cl
-linestyle.use_chaining = False
 lineset.group = bpy.data.groups["sphere_group"]
 
-# line set 8 (spine, silhouette, hidden)
+# line set 8 (sphere, silhouette, hidden)
 lineset = freestyle.linesets["LineSet 8"]
 lineset.select_silhouette = True
 lineset.select_border = False
 lineset.select_crease = False
 lineset.select_by_group = True
-lineset.visibility = 'HIDDEN'
+lineset.visibility = 'RANGE'
+lineset.qi_start = 2
+lineset.qi_end = 100
 linestyle = bpy.data.linestyles["LineStyle.007"]
+linestyle.caps = "ROUND"
+linestyle.color = sp_cl
+linestyle.use_dashed_line = True
+linestyle.dash1 = 5
+linestyle.gap1 = linestyle.dash1
+lineset.group = bpy.data.groups["sphere_group"]
+
+# line set 9 (spine, silhouette, hidden)
+lineset = freestyle.linesets["LineSet 9"]
+lineset.select_silhouette = True
+lineset.select_border = False
+lineset.select_crease = False
+lineset.select_by_group = True
+lineset.visibility = 'RANGE'
+linestyle = bpy.data.linestyles["LineStyle.008"]
 linestyle.caps = "ROUND"
 linestyle.color = [0,0,0]
 linestyle.use_chaining = True
 lineset.group = bpy.data.groups["spine_group"]
+
+# line set 10 (spine, silhouette, hidden)
+lineset = freestyle.linesets["LineSet 10"]
+lineset.select_silhouette = True
+lineset.select_border = False
+lineset.select_crease = False
+lineset.select_by_group = True
+lineset.visibility = 'HIDDEN'
+linestyle = bpy.data.linestyles["LineStyle.009"]
+linestyle.caps = "ROUND"
+linestyle.color = [0,0,0]
+linestyle.use_dashed_line = True
+linestyle.dash1 = 5
+linestyle.gap1 = linestyle.dash1
+lineset.group = bpy.data.groups["spine_group"]
+lineset.show_render = False
+
+
+
+# line set 11 (envelope, silhouette, visible)
+lineset = freestyle.linesets["LineSet 11"]
+lineset.select_silhouette = False
+lineset.select_border = True
+lineset.select_crease = False
+lineset.select_by_group = True
+lineset.visibility = 'VISIBLE'
+linestyle = bpy.data.linestyles["LineStyle.010"]
+linestyle.caps = "ROUND"
+linestyle.color = en_cl
+lineset.group = bpy.data.groups["envelope_group"]
+linestyle.thickness = 1.5
+
+
+# line set 12 (envelope, silhouette, hidden)
+lineset = freestyle.linesets["LineSet 12"]
+lineset.select_silhouette = False
+lineset.select_border = True
+lineset.select_crease = False
+lineset.select_by_group = True
+lineset.visibility = 'HIDDEN'
+linestyle = bpy.data.linestyles["LineStyle.011"]
+linestyle.caps = "ROUND"
+linestyle.color = en_cl
+linestyle.use_dashed_line = True
+linestyle.dash1 = 5
+linestyle.gap1 = linestyle.dash1
+lineset.group = bpy.data.groups["envelope_group"]
+linestyle.thickness = 1.5
+
